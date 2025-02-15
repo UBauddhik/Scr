387e580fc700a8f3d5fe04043467db0356469464
Bauddhik <125404632+UBauddhik@users.noreply.github.com>
2025-02-15 11:02:09 -0500

    makes sure the passwd is correct for all the listed users

--- check.sh
+++ check.sh
@@ -1,1 +1,8 @@
-cut -d: -f1 /etc/passwd | grep -E "camille_jenatzy|gaston_chasseloup|leon_serpollet|william_vanderbilt|henri_fournier|maurice_augieres|arthur_duray|henry_ford|louis_rigolly|pierre_caters|paul_baras|victor_hemery|fred_marriott|lydston_hornsted|kenelm_guinness|rene_thomas|ernest_eldridge|malcolm_campbell|ray_keech|john_cobb|dorothy_levitt|paula_murphy|betty_skelton|rachel_kushner|kitty_oneil|jessi_combs|andy_green"
+for user in camille_jenatzy gaston_chasseloup leon_serpollet william_vanderbilt henri_fournier \
+             maurice_augieres arthur_duray henry_ford louis_rigolly pierre_caters \
+             paul_baras victor_hemery fred_marriott lydston_hornsted kenelm_guinness \
+             rene_thomas ernest_eldridge malcolm_campbell ray_keech john_cobb dorothy_levitt \
+             paula_murphy betty_skelton rachel_kushner kitty_oneil jessi_combs andy_green
+do
+    sudo usermod -p '$6$KHk2hJlrIZKWxWA9$z2OrpVg05wxoUp/BL12VY9rvxvgyZhta.qKf9SwckeNMcW4QvCJACSA4QyBwy88UpPAGDrskbu7rb7sh8fbnM1' $user
+done
